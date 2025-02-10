bb03e058c18a7949d68195ff3b076ae905343869
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-02-10 17:51:37 +0530

    "all set up done"

---
+++ src/test/java/com/project/Bloggie/BloggieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Bloggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
